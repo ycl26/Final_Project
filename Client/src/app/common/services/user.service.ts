5e1cbe95d5ece4577f0f3525576c4680c0705819
ycl26 <yulienc@gmail.com>
2019-12-13 22:27:40 -0500

    fix more lint issues

--- Client/src/app/common/services/user.service.ts
+++ Client/src/app/common/services/user.service.ts
@@ -1,29 +1,28 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
-import { Guest, Candidate, Company, User, UserType, ActiveUser } from '../models/user-model';
+import {Injectable} from '@angular/core';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {Candidate, Company, UserType, ActiveUser} from '../models/user-model';
 
-import { createDefaultGuest, createDefaultUser } from '../factories/user-factory';
-import { HttpClient, HttpResponse } from '@angular/common/http';
-import { map, tap } from 'rxjs/operators';
+import {createDefaultGuest} from '../factories/user-factory';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import {map, tap} from 'rxjs/operators';
 
 const apiUrl = 'http://localhost:3000/api';
 
 type URL = string;
 
-const handleResponse = (response: HttpResponse<Object>) => {
+const handleResponse = (response: HttpResponse<any>) => {
   if (response.status === 200) {
-    return response.body && (<any>response.body).data as any
-    console.log('here')
+    return response.body && (response.body as any).data as any;
   }
   if (response.status === 500) {
     return {
       type: 'Error',
-      errorMessage: response.body && (<any>response.body).errorMessage
+      errorMessage: response.body && (response.body as any).errorMessage
     } as Error;
   }
   return {
     type: 'Error',
-    errorMessage: "server error. contact support"
+    errorMessage: 'server error. contact support'
   } as Error;
 };
 
@@ -36,7 +35,9 @@ export class Error {
   providedIn: 'root'
 })
 export class UserService {
+  // tslint:disable-next-line:variable-name
   _user$ = new BehaviorSubject<ActiveUser>(createDefaultGuest());
+
   constructor(private httpClient: HttpClient) {
     this._updateActiveUser = this._updateActiveUser.bind(this);
   }
@@ -46,12 +47,12 @@ export class UserService {
   }
 
   _getCurrentAuthenticatedUser() {
-    const result$ = this.httpClient.get(`${apiUrl}/userinfo`, { observe: 'response', withCredentials: true});
+    const result$ = this.httpClient.get(`${apiUrl}/userinfo`, {observe: 'response', withCredentials: true});
     return result$.pipe(
       tap((authenticatedUser) => {
         // maybe check for schema
         if (authenticatedUser) {
-          this._user$.next(authenticatedUser as any);
+          this._updateActiveUser(authenticatedUser as any);
         }
       })
     );
@@ -61,19 +62,18 @@ export class UserService {
     return this._user$.asObservable();
   }
 
-
   _updateActiveUser(candidateOrCompany: Candidate | Company) {
-    this._user$.next(<any>candidateOrCompany);
+    this._user$.next(candidateOrCompany);
     return candidateOrCompany;
   }
 
   signup(candidateOrCompany: Candidate | Company): Observable<Candidate | Company | Error> {
     const urlByType = {
       [UserType.Company]: `${apiUrl}/signUpCompany`,
-      [UserType.Candidate]: `${apiUrl}/signUpCandidate`,
+      [UserType.Candidate]: `${apiUrl}/signUpCandidate`
     };
     const url = urlByType[candidateOrCompany.type];
-    const result$ = this.httpClient.post(url, candidateOrCompany, { observe: 'response', withCredentials: true });
+    const result$ = this.httpClient.post(url, candidateOrCompany, {observe: 'response', withCredentials: true});
     return result$.pipe(
       map(handleResponse),
       map(this._updateActiveUser)
@@ -81,29 +81,30 @@ export class UserService {
   }
 
   login(email: string, psw: string): Observable<Candidate | Company | Error> {
-    const result$ = this.httpClient.post(`${apiUrl}/login`, { email, psw }, { observe: 'response', withCredentials: true });
+    const result$ = this.httpClient.post(`${apiUrl}/login`, {email, psw}, {observe: 'response', withCredentials: true});
     return result$.pipe(
       map(handleResponse),
       map(this._updateActiveUser)
-    )
+    );
   }
 
   logout() {
-    const result$ = this.httpClient.post(`${apiUrl}/logout`, { observe: 'response', withCredentials: true });
+    const result$ = this.httpClient.post(`${apiUrl}/logout`, {observe: 'response', withCredentials: true});
     result$.pipe(
       map(handleResponse),
       map(() => {
         const guest = createDefaultGuest();
-        this._user$.next(<any>guest);
+        this._updateActiveUser(guest as any);
         return guest;
       })
-    ).subscribe(() => {})
+    ).subscribe(() => {
+    });
   }
 
   forgotPSW(email: string): Observable<URL> {
-    const result$ = this.httpClient.post(`${apiUrl}/forgotPSW`, { email }, { observe: 'response', withCredentials: true });
+    const result$ = this.httpClient.post(`${apiUrl}/forgotPSW`, {email}, {observe: 'response', withCredentials: true});
     return result$.pipe(
       map(handleResponse)
-    )
+    );
   }
-}  
+}
